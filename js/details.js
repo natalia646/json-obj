ea13cccb445d50ba49023422ff3a744de0eda42e
natalia646 <nataxash46@gmail.com>
2023-02-10 20:07:13 +0200

    post details

--- js/details.js
+++ js/details.js
@@ -1,18 +1,27 @@
 let url = new URL(location.href);
-let dataJSON = url.searchParams.get('data');
+let dataJSON = url.searchParams.get("data");
 let post = JSON.parse(dataJSON);
 
-let h1 = document.createElement('h1');
+let h1 = document.createElement("h1");
 h1.innerText = `${post.id} ${post.title}`;
 
-let p = document.createElement('p');
+let p = document.createElement("p");
 p.textContent = post.body;
 
+let btn = document.createElement("button");
+btn.innerText = "add to favorite";
 
+btn.onclick = function () {
+  let favPostsFromLS = localStorage.getItem("favPosts");
+  if (favPostsFromLS === null) {
+    localStorage.setItem("favPosts", JSON.stringify([]));
+    
+  }
 
-let btn = document.createElement('button');
-btn.innerText = 'add to favorite';
+  let favPostsArray = JSON.parse(favPostsFromLS);
+  favPostsArray.push(post);
+  let stringify = JSON.stringify(favPostsArray);
+  localStorage.setItem("favPosts", stringify);
+};
 
-
-
-document.body.append(h1, p, btn)
+document.body.append(h1, p, btn);
