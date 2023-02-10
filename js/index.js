8942111da5c09c479e16fb6e88a72c1cbd791f2a
natalia646 <nataxash46@gmail.com>
2023-02-10 20:00:41 +0200

    all posts

--- js/index.js
+++ js/index.js
@@ -1,6 +1,16 @@
 fetch("https://jsonplaceholder.typicode.com/posts")
   .then((response) => response.json())
   .then((posts) => {
+
+    let button = document.createElement('button');
+    button.innerText = 'Clear favorites list '
+    document.body.appendChild(button);
+    
+    button.onclick = function(){
+     localStorage.clear()
+    }
+
+
     let ul = document.createElement("ul");
 
     for (const post of posts) {
@@ -18,6 +28,8 @@ fetch("https://jsonplaceholder.typicode.com/posts")
     }
 
     document.body.appendChild(ul);
+
+
   });
 
 // створення html і його додали на сторінку
