eeb1d4ad0ce3442e075d21ca6f38ea7842183a07
natalia646 <nataxash46@gmail.com>
2023-02-12 14:11:55 +0200

    new

--- js/favorites.js
+++ js/favorites.js
@@ -1,8 +1,6 @@
 let favPosts = localStorage.getItem('favPosts');
 let favPostsArray = JSON.parse(favPosts);
 
-console.log(favPostsArray)
-
 for(let post of favPostsArray){
     let h2 = document.createElement('h2');
     h2.innerText = post.id + ' ' + post.title
--- js/index.js
+++ js/index.js
@@ -32,38 +32,5 @@ fetch("https://jsonplaceholder.typicode.com/posts")
 
   });
 
-// створення html і його додали на сторінку
-// for (let i = 0; i < 5; i++) {
-//   let h1 = document.createElement("h1");
-//   h1.innerText = "Lorem dsfwdf fw ww ";
-//   document.body.appendChild(h1);
-// }
-
-// const products = [
-//   {
-//     juice: "Garden",
-//     price: 22,
-//   },
-//   {
-//     juice: "TOM",
-//     price: 32,
-//   },
-//   {
-//     juice: "OUR",
-//     price: 28,
-//   },
-// ];
-
-// for (const product of products) {
-//   let div = document.createElement("div");
-//   let h2 = document.createElement("h2");
-//   h2.innerText = product.juice + " $";
-//   let p = document.createElement("p");
-//   p.innerText = product.price;
-
-//   div.append(h2, p);
-//   document.body.appendChild(div);
-// }
-
 
 
